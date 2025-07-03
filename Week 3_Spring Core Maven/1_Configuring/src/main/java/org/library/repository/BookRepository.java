05f66e342312f9bc9fe5ff7774c3f439f9e861af
Vighnasri Makkena <168977095+vighnasri@users.noreply.github.com>
2025-07-03 23:06:53 +0530

    Delete Week 3_Spring Core Maven directory

--- Week 3_Spring Core Maven/1_Configuring/src/main/java/org/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package org.library.repository;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("Book '" + bookName + "' saved to the repository.");
-    }
-}
--- Week 3_Spring Core Maven/4_Creating_Maven/src/main/java/org/example/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("Book '" + bookName + "' saved to the repository.");
-    }
-}
