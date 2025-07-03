785a475e8a40f49576a84530ecbc7e5a5adf198a
Vighnasri Makkena <168977095+vighnasri@users.noreply.github.com>
2025-07-03 22:01:38 +0530

    Add files via upload

---
+++ Week 3_Spring Core Maven/4_Creating_Maven/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = (BookService) context.getBean("bookService");
+        bookService.addBook("Maven Spring Project");
+    }
+}
